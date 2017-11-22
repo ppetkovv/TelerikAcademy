b4b3b4b055b4a005955772ab0d82fe6c2b75f4f8
Petko <petkotihomirovpetkov@gmail.com>
2017-11-22 18:13:24 +0200

    finishing first task and adding the second one

--- CSharpEntranceExamMyGroup/CSharpEntranceExamMyGroup/Program.cs
+++ CSharpEntranceExamMyGroup/CSharpEntranceExamMyGroup/Program.cs
@@ -12,13 +12,69 @@ namespace CSharpEntranceExamMyGroup
         {
             //First Task
 
-            int year = Int32.Parse(Console.ReadLine());
-            int month = Int32.Parse(Console.ReadLine());
-            int day = Int32.Parse(Console.ReadLine());
+            //int year = Int32.Parse(Console.ReadLine());
+            //int month = Int32.Parse(Console.ReadLine());
+            //int day = Int32.Parse(Console.ReadLine());
 
-            DateTime currentDate = new DateTime(year, month, day);
-            currentDate.AddDays(-1);
-            Console.WriteLine("d-MMM-yyyy", currentDate);
+            //DateTime currentDate = new DateTime(year, month, day);
+            //currentDate = currentDate.AddDays(-1);
+            //string format = "d-MMM-yyyy";
+            //Console.WriteLine(currentDate.ToString(format));
+
+            //Second Task
+
+            //int N = Int32.Parse(Console.ReadLine());
+            //List<int> allPrimeNums = new List<int>();
+            //for (int i = 1; i <= N; i++)
+            //{
+            //    if (FindPrime(i))
+            //    {
+            //        allPrimeNums.Add(i);
+            //    }
+            //}
+
+            //for (int i = 1; i <= allPrimeNums.Count; i++)
+            //{
+            //    for (int j = 1; j <= allPrimeNums[i - 1]; j++)
+            //    {
+            //        if (FindPrime(j))
+            //        {
+            //            Console.Write(1);
+            //        }
+            //        else
+            //        {
+            //            Console.Write(0);
+            //        }
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //Third Task
+
+
+
         }
+
+        //private static bool FindPrime(int i)
+        //{
+        //    int currentPrimeCandidate = i;
+        //    int currentPrimeCandidateSqrt = (int)Math.Sqrt(currentPrimeCandidate);
+        //    bool isPrime = true;
+        //    int divider = 2;
+
+        //    while ((isPrime) && (divider <= currentPrimeCandidateSqrt))
+        //    {
+        //        if (currentPrimeCandidate % divider == 0)
+        //        {
+        //            isPrime = false;
+        //        }
+        //        divider++;
+        //    }
+        //    if (isPrime)
+        //    {
+        //        return true;
+        //    }
+        //    return false;
+        //}
     }
 }
