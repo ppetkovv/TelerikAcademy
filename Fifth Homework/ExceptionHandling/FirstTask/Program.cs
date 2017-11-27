a19ef9336c7a7aca0813e876cd9ff90bf929c813
Petko <petkotihomirovpetkov@gmail.com>
2017-11-27 21:17:57 +0200

    Adding the first and the second task for Exception handling

---
+++ Fifth Homework/ExceptionHandling/FirstTask/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double number = Double.Parse(Console.ReadLine());
+                if(number < 0)
+                {
+                    throw new FormatException();
+                }
+                Console.WriteLine("{0:F3}", Math.Sqrt(number));
+
+            } catch (FormatException ex)
+            {
+                ex = new FormatException("Invalid number!");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
---
+++ Fifth Homework/ExceptionHandling/SecondTask/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.ReadNumber();
+        }
+
+        private static void ReadNumber()
+        {
+            int[] arrayOfNums = new int[10];
+            for (int i = 0; i < arrayOfNums.Length; i++)
+            {
+                arrayOfNums[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            if (!arrayOfNums.OrderBy(x => x).SequenceEqual(arrayOfNums))
+            {
+                throw new FormatException();
+            }
+
+            StringBuilder result = new StringBuilder();
+            result.Append("1 < ");
+            for (int i = 0; i < arrayOfNums.Length; i++)
+            {
+                result.Append(arrayOfNums[i]).Append(" < ");
+            }
+            result.Append("100");
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
