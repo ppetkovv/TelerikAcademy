ae3ab8823335008761ea8544053a81944d7c87ba
Petko <petkotihomirovpetkov@gmail.com>
2018-01-09 15:15:41 +0200

    Submitting the first task from lecture tasks

---
+++ ModuleTwo/HomeworkOne/TaskFromLecture/PrimeNumbersInInterval/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbersInInterval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> allPrimesInInterval = new List<int>();
+            for (int i = 200; i <= 300; i++)
+            {
+                int divider = 2;
+                int currentNumberSqrt = (int)Math.Sqrt(i);
+                bool isPrime = true;
+
+                while (isPrime && divider <= currentNumberSqrt)
+                {
+                    if (i % divider == 0)
+                    {
+                        isPrime = false;
+                    }
+                    divider++;
+                }
+                if (isPrime)
+                {
+                    allPrimesInInterval.Add(i);
+                }
+            }
+            foreach (int num in allPrimesInInterval)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
