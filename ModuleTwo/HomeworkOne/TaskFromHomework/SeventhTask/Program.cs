e886c7ce2e3c06bbec3f6b353eb90aa0fc9f0564
Petko <petkotihomirovpetkov@gmail.com>
2018-01-10 17:29:11 +0200

    Submitting the seventh task from homework tasks

---
+++ ModuleTwo/HomeworkOne/TaskFromHomework/SeventhTask/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeventhTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] someNums = { 3, 4, 4, 2, 3, 3, 4, 3, 2 };
+            List<int> distinctElements = someNums.ToList().Distinct().ToList();
+            SortedDictionary<int, int> dictionary = new SortedDictionary<int, int>();
+
+            for (int i = 0; i < distinctElements.Count; i++)
+            {
+                int currentElementCount = someNums.Where(x => x == distinctElements[i]).Count();
+                if (currentElementCount > 0 && currentElementCount < 1000)
+                {
+                    dictionary.Add(distinctElements[i], currentElementCount);
+                }
+            }
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value} times");
+            }
+
+        }
+    }
+}
