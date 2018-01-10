07a7172dd506ae1763c2cd86dc4bfad691830db2
Petko <petkotihomirovpetkov@gmail.com>
2018-01-10 18:46:20 +0200

    Submiting the nineth task from homework tasks

---
+++ ModuleTwo/HomeworkOne/TaskFromHomework/NinethTask/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinethTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Int32.Parse(Console.ReadLine());
+            List<int> sequence = new List<int>();
+            sequence.Add(N);
+            int currentIndex = 0;
+
+            while (sequence.Count <= 50)
+            {
+                sequence.Add(sequence[currentIndex] + 1);
+                sequence.Add(2 * sequence[currentIndex] + 1);
+                sequence.Add(sequence[currentIndex] + 2);
+                currentIndex++;
+            }
+
+            Console.WriteLine(string.Join(", ", sequence));
+        }
+    }
+}
