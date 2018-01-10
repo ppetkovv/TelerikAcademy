0f58081105e7f83f7597f37867b93fda63aba372
Petko <petkotihomirovpetkov@gmail.com>
2018-01-10 15:44:09 +0200

    Submitting the second task from homework tasks

---
+++ ModuleTwo/HomeworkOne/TaskFromHomework/SecondTask/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Int32.Parse(Console.ReadLine());
+            Stack<int> someNums = new Stack<int>();
+
+            for (int i = 0; i < N; i++)
+            {
+                someNums.Push(Int32.Parse(Console.ReadLine()));
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write(someNums.Pop() + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
