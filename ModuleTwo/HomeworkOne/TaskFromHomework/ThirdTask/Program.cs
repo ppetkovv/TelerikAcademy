0541b620734fa948328bc1309267862e1644cd37
Petko <petkotihomirovpetkov@gmail.com>
2018-01-10 15:54:18 +0200

    Submitting the third task from homework tasks

---
+++ ModuleTwo/HomeworkOne/TaskFromHomework/ThirdTask/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> sequenceOfNums = new List<int>();
+            string currentLine = Console.ReadLine();
+            while (!currentLine.Equals(string.Empty))
+            {
+                sequenceOfNums.Add(Int32.Parse(currentLine));
+                currentLine = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ", sequenceOfNums.OrderBy(x => x)));
+        }
+    }
+}
