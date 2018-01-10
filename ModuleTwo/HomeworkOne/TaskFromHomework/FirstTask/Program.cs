04c83353a99f37870c54654e7e1c8f8c3935263d
Petko <petkotihomirovpetkov@gmail.com>
2018-01-10 15:19:06 +0200

    Submitting the first task from homework tasks

---
+++ ModuleTwo/HomeworkOne/TaskFromHomework/FirstTask/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> sequence = new List<int>();
+            string currentLine = Console.ReadLine();
+            while (!currentLine.Equals(string.Empty))
+            {
+                sequence.Add(Int32.Parse(currentLine));
+                currentLine = Console.ReadLine();
+            }
+            Console.WriteLine($"Sum: {sequence.Sum()}");
+            Console.WriteLine($"Average: {sequence.Average()}");
+        }
+    }
+}
