1f0ecc0da47ad1cb12190c0b35d5e0bbbde45506
Petko <petkotihomirovpetkov@gmail.com>
2018-01-12 11:32:36 +0200

    Submitting the first task from second homework

--- ModuleTwo/HomeworkTwo/Recursion/FirstTask/Program.cs
+++ ModuleTwo/HomeworkTwo/Recursion/FirstTask/Program.cs
@@ -8,9 +8,28 @@ namespace FirstTask
 {
     class Program
     {
+        private static int[] arr;
+        private static int N;
+
         static void Main(string[] args)
         {
+            N = Int32.Parse(Console.ReadLine());
+            arr = new int[N];
+            NestedLoopSimulator(0);
+        }
 
+        private static void NestedLoopSimulator(int currentIndex)
+        {
+            if(currentIndex == arr.Length)
+            {
+                Console.WriteLine(string.Join(" ", arr));
+                return;
+            }
+            for (int i = 1; i <= N; i++)
+            {
+                arr[currentIndex] = i;
+                NestedLoopSimulator(currentIndex+1);
+            }
         }
     }
 }
