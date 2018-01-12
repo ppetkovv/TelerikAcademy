87ba884b5496d184f68d09ead2a33c894fbfdde4
Petko <petkotihomirovpetkov@gmail.com>
2018-01-12 12:55:35 +0200

    Submitting the fourth task from second homework

---
+++ ModuleTwo/HomeworkTwo/Recursion/FourthTask/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace FourthTask
+{
+    class Program
+    {
+        private static int N;
+        private static int[] arr;
+
+        static void Main(string[] args)
+        {
+            N = Int32.Parse(Console.ReadLine());
+            arr = new int[N];
+            CombinationWithDuplicates(0);
+        }
+
+        private static void CombinationWithDuplicates(int currentIndex)
+        {
+            if (currentIndex == arr.Length)
+            {
+                if (arr.Length == arr.ToList().Distinct().Count())
+                    Console.WriteLine(string.Join(" ", arr));
+                return;
+            }
+
+            for (int i = 1; i <= N; i++)
+            {
+                arr[currentIndex] = i;
+                CombinationWithDuplicates(currentIndex + 1);
+            }
+        }
+    }
+}
