c671314afb3cd1ee91f20d5c8638b103c33d955e
Petko <petkotihomirovpetkov@gmail.com>
2018-01-12 12:37:46 +0200

    Submitting the third task from second homework

---
+++ ModuleTwo/HomeworkTwo/Recursion/ThirdTask/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdTask
+{
+    class Program
+    {
+        private static int N;
+        private static int K;
+        private static int[] arr;
+        private static int currentStartIndex = 1;
+
+        static void Main(string[] args)
+        {
+            N = Int32.Parse(Console.ReadLine());
+            K = Int32.Parse(Console.ReadLine());
+            arr = new int[K];
+            CombinationWithDuplicates(0, ref currentStartIndex);
+        }
+
+        private static void CombinationWithDuplicates(int currentIndex, ref int startIndex)
+        {
+            if (currentIndex == arr.Length)
+            {
+                if(arr.Length == arr.ToList().Distinct().Count())
+                    Console.WriteLine(string.Join(" ", arr));
+                return;
+            }
+
+            for (int i = startIndex; i <= N; i++)
+            {
+                arr[currentIndex] = i;
+                CombinationWithDuplicates(currentIndex + 1, ref startIndex);
+            }
+            startIndex = ++currentStartIndex;
+        }
+    }
+}
